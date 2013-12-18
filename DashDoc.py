256093a3e2ce3e70bc51432d5e183db6e8c17b38
Torsten Grust <torsten.grust@gmail.com>
2013-12-18 11:21:33 +0100

    Syntax sensitivity is the default now

--- DashDoc.py
+++ DashDoc.py
@@ -21,7 +21,7 @@ def docset_keys(view, syntax_docset_map):
 
 
 class DashDocCommand(sublime_plugin.TextCommand):
-    def run(self, edit, syntax_sensitive=False):
+    def run(self, edit, flip_syntax_sensitive=False):
         # read global and (project-specific) local settings
         global_settings = sublime.load_settings(__name__ + '.sublime-settings')
         settings  = self.view.settings()
@@ -43,7 +43,7 @@ class DashDocCommand(sublime_plugin.TextCommand):
         query = self.view.substr(selection)
 
         # keys
-        syntax_sensitive = syntax_sensitive ^ syntax_sensitive_as_default
+        syntax_sensitive = flip_syntax_sensitive ^ syntax_sensitive_as_default
         keys = docset_keys(self.view, syntax_docset_map) if syntax_sensitive else []
 
         subprocess.call(['open',
